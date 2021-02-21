fd6600101020d3e9a38dd91c5910d55aa258be40
syahrulrmdhon <syahrulrmdhon2@gmail.com>
2021-02-21 12:58:22 +0700

    add apexcharts for marketInsight component

--- components/dashboard/MarketInsight.js
+++ components/dashboard/MarketInsight.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col } from "antd";
 import {
   DownOutlined,
@@ -8,6 +8,7 @@ import {
 } from "@ant-design/icons";
 import Image from "next/image";
 import styled from "styled-components";
+import Chart from "react-apexcharts";
 
 const CardInsight = styled.div`
   background: #37b04c 0% 0% no-repeat padding-box;
@@ -50,7 +51,75 @@ const ColCustom = styled(Col)`
   margin-top: 5px;
 `;
 
+const CardStats = styled.div`
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 2px 3px #00000029;
+  border-radius: 2px;
+  padding: 10px 20px;
+  margin: 20px 15px 0 0;
+`;
+
 const MarketInsight = ({ collapsed, setCollapsed }) => {
+  const [state, setState] = useState({
+    series: [
+      {
+        name: "Website Blog",
+        type: "column",
+        data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160],
+      },
+      {
+        name: "Social Media",
+        type: "line",
+        data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+      },
+      stroke: {
+        width: [0, 4],
+      },
+      title: {
+        text: "Traffic Sources",
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: [1],
+      },
+      labels: [
+        "01 Jan 2001",
+        "02 Jan 2001",
+        "03 Jan 2001",
+        "04 Jan 2001",
+        "05 Jan 2001",
+        "06 Jan 2001",
+        "07 Jan 2001",
+        "08 Jan 2001",
+        "09 Jan 2001",
+        "10 Jan 2001",
+        "11 Jan 2001",
+        "12 Jan 2001",
+      ],
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: [
+        {
+          title: {
+            text: "Website Blog",
+          },
+        },
+        {
+          opposite: true,
+          title: {
+            text: "Social Media",
+          },
+        },
+      ],
+    },
+  });
   return (
     <React.Fragment>
       <CardInsight onClick={() => setCollapsed(!collapsed)}>
@@ -76,30 +145,51 @@ const MarketInsight = ({ collapsed, setCollapsed }) => {
         </Row>
       </CardInsight>
       {collapsed && (
-        <CardSales>
+        <React.Fragment>
+          <CardSales>
+            <Row>
+              <Col span={22}>
+                <p>Sales Turnover</p>
+              </Col>
+              <Col span={2}>
+                <MoreOutlined />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={20}>
+                <h3>Rp. 3,600,000</h3>
+                <p className="stats">
+                  <strong>
+                    <FallOutlined /> 13.8%
+                  </strong>{" "}
+                  last period in products sold
+                </p>
+              </Col>
+              <Col span={4}>
+                <Image src="/sales.svg" height={49} width={43} />
+              </Col>
+            </Row>
+          </CardSales>
           <Row>
-            <Col span={22}>
-              <p>Sales Turnover</p>
+            <Col span={14}>
+              <CardStats>
+                Stats
+                <Chart
+                  options={state.options}
+                  series={state.series}
+                  type="line"
+                  height={350}
+                />
+              </CardStats>
             </Col>
-            <Col span={2}>
-              <MoreOutlined />
+            <Col span={5}>
+              <CardStats>Top Sell</CardStats>
+            </Col>
+            <Col span={5}>
+              <CardStats>Top Competitors</CardStats>
             </Col>
           </Row>
-          <Row>
-            <Col span={20}>
-              <h3>Rp. 3,600,000</h3>
-              <p className="stats">
-                <strong>
-                  <FallOutlined /> 13.8%
-                </strong>{" "}
-                last period in products sold
-              </p>
-            </Col>
-            <Col span={4}>
-              <Image src="/sales.svg" height={49} width={43} />
-            </Col>
-          </Row>
-        </CardSales>
+        </React.Fragment>
       )}
     </React.Fragment>
   );
